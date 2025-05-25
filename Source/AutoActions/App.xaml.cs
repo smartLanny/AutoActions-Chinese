e49bb06cbe14805a4baa633c7c6fc8c0f74ba6eb
smartLanny <51014844+smartLanny@users.noreply.github.com>
2025-05-26 00:49:52 +0800

    修改应用程序的入口点，在应用程序启动时设置默认语言为中文

--- Source/AutoActions/App.xaml.cs
+++ Source/AutoActions/App.xaml.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,14 +30,47 @@ namespace AutoActions
             mutex = new Mutex(true, "{2846416C-610B-4A6B-A31C-A4AA6826E9BE}", out createNew);
             if (mutex.WaitOne(TimeSpan.Zero, true))
             {
+                // 设置默认语言为中文
+                SetDefaultLanguage("zh-CN");
+
                 var application = new App();
                 application.InitializeComponent();
                 Globals.Instance.LoadSettings();
+
+                // 从设置中加载已保存的语言设置
+                if (!string.IsNullOrEmpty(Globals.Instance.Settings.SelectedLanguage))
+                {
+                    SetDefaultLanguage(Globals.Instance.Settings.SelectedLanguage);
+                }
+
                 application.Run();
             }
             else
             {
                 MessageBox.Show(ProjectLocales.AlreadyRunning);
+            }
+        }
+
+        private static void SetDefaultLanguage(string cultureName)
+        {
+            try
+            {
+                CultureInfo culture = new CultureInfo(cultureName);
+                ProjectLocales.Culture = culture;
+                
+                // 设置当前线程和默认线程的文化信息
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture.Name);
+                Thread.CurrentThread.CurrentUICulture = culture;
+
+                // 输出日志信息
+                Globals.Logs.Add($"应用语言: {culture.DisplayName}", false);
+            }
+            catch (Exception ex)
+            {
+                // 语言设置失败，使用默认语言
+                Globals.Logs.AddException(ex);
             }
         }
 
