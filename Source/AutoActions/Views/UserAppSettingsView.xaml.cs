b86711c57f8e7ced21825416fffe6f68f36f5e69
smartLanny <51014844+smartLanny@users.noreply.github.com>
2025-05-26 00:52:24 +0800

    添加语言选择相关的逻辑

--- Source/AutoActions/Views/UserAppSettingsView.xaml.cs
+++ Source/AutoActions/Views/UserAppSettingsView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace AutoActions.Views
@@ -11,13 +14,67 @@ namespace AutoActions.Views
         public UserAppSettingsView()
         {
             InitializeComponent();
+            Loaded += UserAppSettingsView_Loaded;
+        }
+
+        private void UserAppSettingsView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is UserAppSettings settings)
+            {
+                // 设置初始选中的语言项
+                var comboBox = GetLanguageComboBox();
+                if (comboBox != null)
+                {
+                    foreach (ComboBoxItem item in comboBox.Items)
+                    {
+                        if (item.Tag.ToString() == settings.SelectedLanguage)
+                        {
+                            comboBox.SelectedValue = item.Tag.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private ComboBox GetLanguageComboBox()
+        {
+            // 查找语言下拉框控件
+            ComboBox languageComboBox = null;
+            
+            // 使用名称查找
+            var grid = this.Content as Grid;
+            if (grid != null)
+            {
+                var scrollViewer = grid.Children[2] as ScrollViewer;
+                if (scrollViewer != null)
+                {
+                    var innerGrid = scrollViewer.Content as Grid;
+                    if (innerGrid != null)
+                    {
+                        foreach (var child in innerGrid.Children)
+                        {
+                            if (child is ComboBox comboBox)
+                            {
+                                // 查找绑定到SelectedLanguage的ComboBox
+                                var binding = comboBox.GetBindingExpression(ComboBox.SelectedValueProperty);
+                                if (binding != null && binding.ParentBinding.Path.Path == "SelectedLanguage")
+                                {
+                                    languageComboBox = comboBox;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return languageComboBox;
         }
 
         private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
-
-
     }
 }
